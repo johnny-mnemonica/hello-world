aed7de4ad06360c68658c1b2adc2219cc47e16b6
Monica Rekas <101420263+johnny-mnemonica@users.noreply.github.com>
2026-05-11 19:47:28 -0400

    spring clean

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,64 +2,90 @@ import { EleventyRenderPlugin } from "@11ty/eleventy";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import { minify } from "terser";
 import CleanCSS from "clean-css";
+import htmlmin from "html-minifier-terser";
 
 export default async function(eleventyConfig) {
 
-  // Render Plugin
+  //// ** ----------------------- PLUGINS ----------------------- 
+  
+  // * ----------- Plugin: Render [bundled w/ 11ty core]
   eleventyConfig.addPlugin(EleventyRenderPlugin);
-
-  // Image Plugin
+  
+  // * ----------- Plugin: Image [@11ty/image]
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
-
+    
     formats: ["webp", "jpeg"],
     widths: [550, 800, 1000, 1200, 1600, 2000],
-
+    
 		// optional, attributes assigned on <img> nodes override these values
 		htmlOptions: {
-			imgAttributes: {
-				loading: "lazy",
+      imgAttributes: {
+        loading: "lazy",
         sizes: '100vw',
 				decoding: "async",
 			},
 			pictureAttributes: {}
 		},
   });
+  
+  //// ** -----------------------  MINIFY ----------------------- 
 
-  // Public assets
+  // * ----------- JS [terser/minify]
+  eleventyConfig.addFilter("jsmin", async function (code) {
+    try {
+      const minified = await minify(code);
+      return minified.code;
+    } catch (err) {
+      console.error("Terser error: ", err);
+    }
+  });
+  
+  // * ----------- CSS [clean-css/CleanCSS]
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+  
+  // * ----------- HTML [html-minifier-terser/htmlmin]
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if ((this.page.outputPath || "").endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+  
+    // If not an HTML output, return content as-is
+    return content;
+  });
+
+  //// ** -----------------------  PASSTHROUGH COPIES ----------------------- 
+  // * Files copied to their appropriate output directories.
+
+  // * ----------- Public assets 
   eleventyConfig.addPassthroughCopy({
     "./public/": "/"
   });
 
-  // Swiper.js
+  // * ----------- Swiper.js [JS]
   eleventyConfig.addPassthroughCopy({
     "./node_modules/swiper/swiper-bundle.min.js": "/scripts/swiper.min.js"
   });
 
+  // * ----------- Swiper.js [CSS]
   eleventyConfig.addPassthroughCopy({
     "./node_modules/swiper/swiper.min.css": "/css/swiper.min.css"
   });
 
-  // Font Face Observer
+  //* ----------- Font Face Observer
   eleventyConfig.addPassthroughCopy({
     "./node_modules/fontfaceobserver/fontfaceobserver.js": "/scripts/fontfaceobserver.js"
   });
 
-  // Minify JS
-  eleventyConfig.addFilter("jsmin", async function (code) {
-		try {
-			const minified = await minify(code);
-			return minified.code;
-		} catch (err) {
-			console.error("Terser error: ", err);
-		}
-	});
+  //// ** -----------------------  THAT'S ALL, FOLKS ----------------------- 
+  // * Return your Object options:
 
-  // Minify CSS
-  eleventyConfig.addFilter("cssmin", function (code) {
-		return new CleanCSS({}).minify(code).styles;
-	});
-
-  // Return your Object options:
   return {
     dir: {
       input: "src",
